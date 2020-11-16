ac5603613e748a26547af316d40b56461a012c79
kazutoshi nagai <kazutoshi666_export@yahoo.co.jp>
2020-11-16 16:09:01 +0900

    First commit

---
+++ homepage.php
@@ -0,0 +1,231 @@
+<?php
+
+include "connection.php";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Homepage</title>
+</head>
+  <div class="overflow-auto" style="">
+<body>
+  <nav class="navbar navbar-expand-lg navbar-primary bg-light">
+
+  <a href="#" class="navbar-brand">
+    <h1 class="h3">EEM site</h1>
+  </a>
+
+  <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse"id="mainNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a href="#" class="nav-link">Posts</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Add Post</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Help</a>
+      </li>
+    </ul> 
+
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link">Site Map</a>
+      </li>
+      <li class="nav-item">
+        <button class="btn btn-outline-danger rounded-pill  font-weight-bold" style="width:90px;">Join Us</button>
+      </li>
+    </ul> 
+  </div>    
+  </nav>
+
+  <div class="bg-light">
+  <header>
+    <div class="jumbotron jumbotron-fluid">   
+      <div class="float-left inline ml-3"><h2 class="h5 text-info"><i class="fas fa-book mr-2"></i>English Education Material Website</h2></div>
+      <div class="text-center">
+     <input type="text" placeholder="search bar" name="searchbar" id="" class="col-6"><a class="btn btn-primary btn-sm ml-2" href="item.php" title="search"><i class="fas fa-search mr-2"></i>Search</a><a class="btn btn-primary btn-sm ml-2" href="cart.php" title="cart"><i class="fas fa-shopping-cart mr-2"></i>Cart</a>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    
+    <div class="container float-left col-3">
+      <div class="card">
+        <div class="card-body">
+        
+        <div class="card">
+            <div class="card-header">About Us</div>
+              <div class="card-body">
+              <div class="card-text">we will be providing you the most effincial english materials</div>
+          </div>
+        </div>  
+
+        <div class="card mt-3">
+          <div class="card-header">Information of our materials</div>
+            <div class="card-body">
+              <div class="card-title">cd</div>
+              <div class="card-text"></div>
+            </div>
+        </div>  
+
+        </div>
+      </div>
+    </div>
+  
+    <div class="container">
+      <div class="card">
+        <div class="card-deck mx-3 my-3">
+        
+        <div class="card">
+        <img src="..." class="card-img-top" alt="book">
+          <div class="card-body">
+            <div class="card-title">cd</div>
+            <div class="card-text"></div>
+          </div>
+        </div>  
+                    
+        <div class="card">
+        <img src="..." class="card-img-top" alt="book">
+          <div class="card-body">
+              <div class="card-title">cd</div>
+              <div class="card-text"></div>
+            </div>
+        </div>    
+
+        <div class="card">
+        <img src="..." class="card-img-top" alt="book">
+          <div class="card-body">
+              <div class="card-title">cd</div>
+            </div>
+        </div>    
+
+        <div class="card">
+        <img src="..." class="card-img-top" alt="book">
+          <div class="card-body">
+              <div class="card-title">cd</div>
+            </div>
+        </div>    
+
+        <div class="card">
+        <img src="..." class="card-img-top" alt="book">
+          <div class="card-body">
+              <div class="card-title">cd</div>
+            </div>
+        </div>    
+
+        <div class="card">
+        <img src="..." class="card-img-top" alt="book">
+          <div class="card-body">
+              <div class="card-title">cd</div>
+            </div>
+        </div>   
+
+        <div class="card">
+        <img src="..." class="card-img-top" alt="book">
+          <div class="card-body">
+              <div class="card-title">cd</div>
+            </div>
+        </div>    
+        
+        </div>                    
+      </div>
+    </div>                   
+
+    <div class="container my-3">
+      <div class="card">
+        <div class="card-deck mx-3 my-3">
+
+          <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <div class="card-title">book</div>
+            </div>
+          </div>
+
+          <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <div class="card-title">book</div>
+            </div>
+          </div>
+
+          <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <div class="card-title">book</div>
+            </div>
+          </div>
+
+          <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <div class="card-title">book</div>
+            </div>
+          </div>
+
+          <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <div class="card-title">book</div>
+            </div>
+          </div>
+
+          <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <div class="card-title">book</div>
+            </div>
+          </div>
+
+          <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <div class="card-title">book</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container my-3">
+      <div class="card border" style="form-control">
+      <div class="card-body">
+        advertisement
+      </div>
+      </div>
+    </div>
+
+  </main>
+
+  <footer class="bg-info text-center w-100" style="margin-top: 200px;">
+    <div class="footer-content">
+      <small style="line-height:100px;">www.English Education Material.com</small>
+    </div>
+  </footer>
+
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
